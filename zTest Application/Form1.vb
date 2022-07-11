072c75b30144ab24f1a1d8f5d100478be94061d3
Rômulo Meirelles <52598055+Romulo-Meirelles@users.noreply.github.com>
2022-07-10 22:36:03 -0300

    Add files via upload

---
+++ zTest Application/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports ToolsBox
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Application.DoEvents()
+        Dim PanelGradient As New Panel_Gradient
+        PanelGradient.Dock = DockStyle.Fill
+        PanelGradient.GradientBottomLeft = Color.Black
+        PanelGradient.GradientBottomRight = Color.Coral
+        PanelGradient.GradientTopLeft = Color.DarkRed
+        PanelGradient.GradientTopRight = Color.DarkRed
+        Me.Controls.Add(PanelGradient)
+        Application.DoEvents()
+    End Sub
+End Class
